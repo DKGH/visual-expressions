ffe53dc660fb770d17bb0854996a9b9866458ade
Dheeraj Kumar <dheeraj.iitm@gmail.com>
2026-03-29 16:13:30 -0700

    Make cube shiney

--- src/components/Cube/Cube.tsx
+++ src/components/Cube/Cube.tsx
@@ -3,7 +3,7 @@ import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import initManifold, { ManifoldToplevel } from 'manifold-3d';
 import { Manifold } from 'manifold-3d/manifoldCAD.js';
-import { Text } from '@mantine/core';
+import * as THREE from 'three';
 
 export function Cube() {
   const [manifold, setManifold] = useState<ManifoldToplevel | null>(null);
@@ -23,9 +23,10 @@ export function Cube() {
   }
 
   return (
-    <Canvas style={{ height: '100vh', width: '100vw', background: 'red' }}>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+    <Canvas style={{ height: '100vh', width: '100vw' }}>
+      <ambientLight intensity={0.2} />
+      <pointLight position={[10, 10, 10]} intensity={1.4} />
+      <directionalLight position={[-4, 4, 4]} intensity={1} />
       <ManifoldObject />
       <OrbitControls />
     </Canvas>
@@ -33,45 +34,23 @@ export function Cube() {
 }
 
 export function ManifoldObject() {
-  const meshData = useMemo(() => {
+  const geometry = useMemo(() => {
     const { cube } = Manifold;
     const box = cube([1, 1, 1], true);
     const mesh = box.getMesh();
     box.delete();
-    return mesh;
+
+    const geo = new THREE.BufferGeometry();
+    geo.setAttribute('position', new THREE.BufferAttribute(mesh.vertProperties, 3));
+    geo.setIndex(new THREE.BufferAttribute(mesh.triVerts, 1));
+    geo.computeVertexNormals();
+
+    return geo;
   }, []);
 
-  if (!meshData) {
-    return <Text c="dimmed">Error loading 3D model. Check console for details.</Text>;
-  }
-
-  // 3. Return a React Three Fiber element
-  const positions = new Float32Array(meshData.numVert * 3);
-  for (let i = 0; i < meshData.numVert; i++) {
-    positions[i * 3] = meshData.vertProperties[i * meshData.numProp];
-    positions[i * 3 + 1] = meshData.vertProperties[i * meshData.numProp + 1];
-    positions[i * 3 + 2] = meshData.vertProperties[i * meshData.numProp + 2];
-  }
-
   return (
-    <mesh>
-      <bufferGeometry>
-        <bufferAttribute
-          attach="attributes-position"
-          array={positions}
-          args={[positions, 3]}
-          count={meshData.numVert}
-          itemSize={3}
-        />
-        <bufferAttribute
-          attach="index"
-          array={meshData.triVerts}
-          args={[meshData.triVerts, 1]}
-          count={meshData.triVerts.length}
-          itemSize={1}
-        />
-      </bufferGeometry>
-      <meshStandardMaterial color="orange" />
+    <mesh geometry={geometry}>
+      <meshStandardMaterial color="orange" roughness={0.4} metalness={0.1} />
     </mesh>
   );
 }
